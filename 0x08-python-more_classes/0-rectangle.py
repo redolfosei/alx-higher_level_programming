2f784912c49241aabb8a7dcdea944a5ebb6d1dd9
redolfosei <redolf639@gmail.com>
2023-04-06 18:52:50 +0000

    Python More Classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""This defines a Rectangle Class."""
+class Rectangle:
+    """This Class represents a rectangle."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+0. an empty class Rectangle that defines a rectangle:
+
+   You are not allowed to import any module
+
+1.
