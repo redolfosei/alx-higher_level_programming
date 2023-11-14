e24fda45dc867aab99e7e72b7cc5d894346d5f10
redolfosei <redolf639@gmail.com>
2023-11-14 00:31:06 +0000

    js warm up 0 - 11

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const loveC = 'C is fun'
-const x = process.argv[2]
+const loveC = 'C is fun';
+const x = process.argv[2];
 for (let i = 0; i < x; i++) {
-  console.log(loveC)
+  console.log(loveC);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const sqr = process.argv[2]
+const sqr = process.argv[2];
 if (isNaN(sqr)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   for (let x = 0; x < sqr; x++) {
-    console.log('X'.repeat(sqr))
+    console.log('X'.repeat(sqr));
   }
 }
