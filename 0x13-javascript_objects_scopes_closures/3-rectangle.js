4ae9bbdd723cba94185f8e1ec6abcd51a14e68c4
redolfosei <redolf639@gmail.com>
2023-11-14 22:03:47 +0000

    js warm up objects

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,9 +4,9 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) {
       [this.width, this.height] = [w, h];
     }
-  }
+  };
 
   print() {
-    for (let i = 0; i < this.height; i++) console.log("X".repeat(this.width));
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
